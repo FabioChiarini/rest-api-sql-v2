774bf3c3d0266e0d496c5b5fe64914cd458b4099
Fabio <chiarrss@gmail.com>
2019-07-19 07:51:12 +0200

    Setting up models

---
+++ models/course.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define('Course', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    estimatedTime: DataTypes.STRING,
+    materialsNeeded: DataTypes.STRING
+  }, {});
+  Course.associate = function(models) {
+    Course.belongsTo(models.User, { foreignKey: {
+      fieldName: 'userId',
+      allowNull: true,
+    },
+  });
+  };
+  return Course;
+};
---
+++ models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id:  {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {});
+  User.associate = function(models) {
+    User.hasMany(models.Course, { foreignKey: {
+      fieldName: 'userId',
+      allowNull: true,
+    },
+  });
+  };
+  return User;
+};
