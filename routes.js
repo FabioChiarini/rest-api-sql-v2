8b1b15ea58e4ad95205eff465e2aef291529fed1
Fabio <chiarrss@gmail.com>
2019-07-25 07:45:11 +0200

    First draft of authentication

--- routes.js
+++ routes.js
@@ -5,15 +5,64 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
 
 var User = require("./db").User;
 
-router.get("/users", (req, res, next) => {
+
+const authenticateUser = (req, res, next) => {
+  let message = null;
+  const credentials = auth(req);
+  if (credentials) {
+    User.findAll().then(users => {
+      const user = users.find(u => u.emailAddress === credentials.name)
+      if(user) {
+      
+        const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
+  
+  
+        if (authenticated) {
+          console.log(`Welcome back dear ${user.username}`)
+          req.currentUser = user;
+        } else {
+            message = `Authentication failure for username: ${user.username}`;
+        }
+        
+      } else {
+        message = `User ${user.username} not found`;
+      }
+    });
+    
+
+
+  } else {
+      message = `Auth header not found`;
+    }
+
+    if (message) {
+      console.warn(message);
+      res.status(401).json({
+        message: 'ACCESS DENIED'
+      });
+    } else {
+      next();
+    }
+  }
+
+
+router.get("/users", authenticateUser, (req, res, next) => {
+  const user = req.currentUser;
+
+  res.JSON({
+    name: user.name,
+    username: user.username
+  })
+  /*
   User.findAll({ raw: true }).then(users => {
     console.log(users);
 
     res.status(200).redirect("/");
-  });
+  });*/
 });
 
 router.post("/users", [
