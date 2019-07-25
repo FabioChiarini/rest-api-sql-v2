dca56009a8590aaf300b3916b4f7a42eaf50f9b0
Fabio <chiarrss@gmail.com>
2019-07-25 07:06:54 +0200

    Added standard validation errors on new users

--- routes.js
+++ routes.js
@@ -3,22 +3,47 @@
 const express = require("express");
 
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
 var User = require("./db").User;
 
 router.get("/users", (req, res, next) => {
+
+/*
   let users_list = [];
   User.findAll({ raw: true }).then(users => {
-    //console.log(users[0]);
-    for (user in users) {
-      console.log(user.id);
-      users_list.push(user);
+    console.log(users.length);
+    var users_parse = JSON.parse(users)
+    console.log(users_parse);
+    /*
+    let i;
+    for (i = 0; i < users.lenth; i += 1){
+      console.log(users[i]);
+      users_list.push(users[i]);
     }
     res.json(users_list);
-  });
+  });*/
 });
 
-router.post("/users", (req, res, next) => {
+router.post("/users", [
+
+  check('firstName').exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "firstName"'),
+  check('lastName').exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "lastName"'),
+  check('emailAddress').exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "emailAddress"'),
+  check('password').exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "password"'),
+
+], (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(error => error.msg);
+    return res.status(400).json({ errors: errorMessages });
+
+  }
   const user = req.body;
   console.log(user);
   User.create(user)
