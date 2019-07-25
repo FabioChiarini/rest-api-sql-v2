e6b97d459155b92e688f753e4582d385d6e68f0a
Fabio <chiarrss@gmail.com>
2019-07-25 07:19:06 +0200

    Added password hashing on new users

--- routes.js
+++ routes.js
@@ -3,26 +3,17 @@
 const express = require("express");
 
 const router = express.Router();
-const { check, validationResult } = require('express-validator')
+const { check, validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
 
 var User = require("./db").User;
 
 router.get("/users", (req, res, next) => {
-
-/*
-  let users_list = [];
   User.findAll({ raw: true }).then(users => {
-    console.log(users.length);
-    var users_parse = JSON.parse(users)
-    console.log(users_parse);
-    /*
-    let i;
-    for (i = 0; i < users.lenth; i += 1){
-      console.log(users[i]);
-      users_list.push(users[i]);
-    }
-    res.json(users_list);
-  });*/
+    console.log(users);
+
+    res.status(200).redirect("/");
+  });
 });
 
 router.post("/users", [
@@ -45,7 +36,7 @@ router.post("/users", [
 
   }
   const user = req.body;
-  console.log(user);
+  user.password = bcryptjs.hashSync(user.password);
   User.create(user)
     .then(() => {
       res.status(201).redirect("/");
